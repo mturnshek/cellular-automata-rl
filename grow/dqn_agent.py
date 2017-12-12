b11fee44c5c08ddd218beca20cd9d1cbbebc1db2
Matt Turnshek <matthewturnshek@gmail.com>
2017-12-12 16:07:41 -0500

    remove local keras-rl and fix dqn convergence

--- grow/dqn_agent.py
+++ grow/dqn_agent.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 from keras.models import Model
-from keras.layers import Dense, Reshape, Activation, Flatten, Input, Conv2D, concatenate, Dropout
+from keras.layers import Dense, Reshape, Flatten, Input, Conv2D, concatenate, GaussianNoise, BatchNormalization
 from keras.optimizers import RMSprop
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 from rl.processors import MultiInputProcessor
 
@@ -21,8 +21,12 @@ def create_input(shape):
 
 class DQN_Agent():
     def __init__(self, display=False):
-        self.env = Environment(display=False)
+        self.env = Environment(display=display)
         self.nb_actions = self.env.rows*self.env.cols + 1
+
+        self.model_save_path = 'weights/grow_dqn_small_weights.h5'
+        self.log_save_path = 'logs/grow_dqn_small_performance.npy'
+        self.episode_reward_log = []
 
         self.create_model()
         self.initialize_dqn()
@@ -32,8 +36,8 @@ class DQN_Agent():
         move_tile_counter_shape = self.env.x2_shape
         round_counter_shape = self.env.x3_shape
 
-        # Keras-rl interprets an extra axis=0 dimension
-        # tacked on to observations
+        # Keras-rl interprets an extra dimension at axis=0
+        # tacked on to our observations, which we take into account here
         board_input = Input((1,) + board_shape)
         move_tile_counter_input = Input((1,) + move_tile_counter_shape)
         round_counter_input = Input((1,) + round_counter_shape)
@@ -43,17 +47,13 @@ class DQN_Agent():
         round_counter = Reshape(round_counter_shape)(round_counter_input)
 
         ### Neural network
-        cnn = Conv2D(96, (5, 5), padding='same', activation='relu')(board)
-        cnn = Conv2D(96, (5, 5), padding='same', activation='relu')(cnn)
-        cnn = Conv2D(96, (5, 5), padding='same', activation='relu')(cnn)
+        cnn = Conv2D(64, (5, 5), padding='same', activation='relu')(board)
         flat_cnn = Flatten()(cnn)
 
         denses = concatenate([flat_cnn, move_tile_counter, round_counter])
-        denses = Dense(512, activation='relu')(denses)
-        denses = Dropout(0.3)(denses)
-        denses = Dense(512, activation='relu')(denses)
-        denses = Dropout(0.3)(denses)
-        predictions = Dense(self.nb_actions, activation='softmax')(denses)
+        denses = Dense(128, activation='relu')(denses)
+
+        predictions = Dense(self.nb_actions, kernel_initializer='zeros', activation='linear')(denses)
         ###
 
         model = Model(
@@ -64,24 +64,34 @@ class DQN_Agent():
         self.model = model
 
     def initialize_dqn(self):
-        memory = SequentialMemory(limit=50000, window_length=1)
-        policy = BoltzmannQPolicy()
+        memory = SequentialMemory(limit=1000000, window_length=1)
+        self.policy = BoltzmannQPolicy()
         processor = MultiInputProcessor(nb_inputs=3)
         self.dqn = DQNAgent(model=self.model, nb_actions=self.nb_actions,
-                            memory=memory, nb_steps_warmup=10,
-                            processor=processor,
-                            target_model_update=1, policy=policy)
+                            memory=memory, nb_steps_warmup=10, gamma=0.95,
+                            processor=processor, enable_dueling_network=True,
+                            target_model_update=500, policy=self.policy)
         self.dqn.compile(RMSprop())
 
     def train(self):
-        self.dqn.fit(self.env, nb_steps=900000, log_interval=50)
-        self.save()
+        steps = 200
+        cycles = 10000
+        for i in range(cycles):
+            history = self.dqn.fit(self.env, nb_steps=steps, log_interval=50)
+            # save current episode reward
+            episode_rewards = history.history['episode_reward']
+            self.episode_reward_log.append((i*steps, np.mean(episode_rewards)))
 
-    def load(self):
-        self.dqn.load_weights('weights/grow_dqn_weights.h5')
+            if i % 50 == 0:
+                print("Saving model and performance log ...")
+                self.save(self.model_save_path)
+                np.save(self.log_save_path, self.episode_reward_log)
 
-    def save(self):
-        self.dqn.save_weights('weights/grow_dqn_weights.h5', overwrite=True)
+    def load(self, path):
+        self.dqn.load_weights(path)
+
+    def save(self, path):
+        self.dqn.save_weights(path, overwrite=True)
 
     def test(self):
         self.dqn.test(env, nb_episodes=50000)
